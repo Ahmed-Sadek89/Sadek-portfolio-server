d63cabedf58a0a0b102883cdeb138223797cb5bb
Ahmed-Sadek89 <ahmeamohamed161@gmail.com>
2024-08-09 15:21:13 +0200

    remove the upload to disk storage

--- src/config/multer.config.ts
+++ src/config/multer.config.ts
@@ -1,26 +1,5 @@
 import multer from 'multer';
-import path from 'path';
-import * as dotenv from 'dotenv';
-dotenv.config()
-// Determine the upload destination based on the environment
-const uploadPath =
-    process.env.NODE_ENV === 'production'
-        ? `${process.env.BACKEND_LINK}/uploads/`  // Set your production path here
-        : 'uploads/';  // Local development path
 
-const storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-        cb(null, uploadPath);
-    },
-    filename: function (req, file, cb) {
-        // Extract the original file extension
-        const extension = path.extname(file.originalname);
-
-        // Create a custom name with the extension
-        const customFileName = `IMG-${Date.now()}${extension}`;
-
-        cb(null, customFileName);
-    }
+export const upload = multer({
+    storage: multer.diskStorage({})
 });
-
-export const upload = multer({ storage });
